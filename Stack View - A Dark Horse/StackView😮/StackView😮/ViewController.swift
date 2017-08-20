ecf517187736163faebaf9a9b15b275325f34540
Kuliza-369 <mohit.anand@kuliza.com>
2017-08-21 02:59:35 +0530

    project setup done

---
+++ Stack View - A Dark Horse/StackView😮/StackView😮/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView😮
+//
+//  Created by Kuliza-369 on 21/08/17.
+//  Copyright © 2017 Kuliza-369. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
