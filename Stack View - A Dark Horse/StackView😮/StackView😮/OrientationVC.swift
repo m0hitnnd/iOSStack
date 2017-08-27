99173bb5c15d435789a366f52bd7e12fb5679501
Kuliza-369 <mohit.anand@kuliza.com>
2017-08-28 02:27:29 +0530

    adding orientation magic

---
+++ Stack View - A Dark Horse/StackView😮/StackView😮/OrientationVC.swift
@@ -0,0 +1,30 @@
+//
+//  OrientationVC.swift
+//  StackView😮
+//
+//  Created by Kuliza-369 on 28/08/17.
+//  Copyright © 2017 Kuliza-369. All rights reserved.
+//
+
+import UIKit
+
+class OrientationVC: UIViewController {
+
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeAlignmentTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.5) { [unowned self] in
+            if self.stackView.axis == .horizontal {
+                self.stackView.axis = .vertical
+            } else {
+                self.stackView.axis = .horizontal
+            }
+        }
+    }
+}
--- Stack View - A Dark Horse/StackView😮/StackView😮/ViewController.swift
+++ Stack View - A Dark Horse/StackView😮/StackView😮/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    fileprivate var dataSource = ["Orientation Magic"]
+    
+    fileprivate let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +32,44 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "defaultCell"
+        
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell?.textLabel?.text = dataSource[indexPath.row]
+        
+        return cell!
+        
+    }
+}
+
+extension ViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.row {
+        case 0:
+            let orientationVc = mainStoryboard.instantiateViewController(withIdentifier: "OrientationVC")
+            navigationController?.pushViewController(orientationVc, animated: true)
+            
+        default:
+            print("invalide case")
+        }
+    }
 }
 
