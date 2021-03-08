824ff668c7d0557930a8bdfe38216aca30dc028d
m0hitnnd <mohitanand95.ma@gmail.com>
2021-03-08 17:40:45 +0530

    indentation

--- CustomTernaryOperator/CustomTernaryOperator.playground/Contents.swift
+++ CustomTernaryOperator/CustomTernaryOperator.playground/Contents.swift
@@ -4,11 +4,17 @@ infix operator !: TernaryPrecedence
 infix operator |: TernaryPrecedence
 
 
-func |<T1, T2>(lhs: @escaping (T1) -> T2, rhs: @autoclosure @escaping () -> T2) -> (second: (T1) -> T2, third: () -> T2) {
+func |<T1, T2>(
+    lhs: @escaping (T1) -> T2,
+    rhs: @autoclosure @escaping () -> T2
+) -> (second: (T1) -> T2, third: () -> T2) {
     return (lhs, rhs)
 }
 
-func !<T1, T2>(lhs: Optional<T1>, rhs: (second: (T1) -> T2, third: () -> T2)) -> T2 {
+func !<T1, T2>(
+    lhs: Optional<T1>,
+    rhs: (second: (T1) -> T2, third: () -> T2)
+) -> T2 {
     guard let unwrappedLhs = lhs else {
         return rhs.third()
     }
